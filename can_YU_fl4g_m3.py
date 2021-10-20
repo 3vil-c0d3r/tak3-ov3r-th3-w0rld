2277b4887250fc5b8d067e3d9cf19566516d54f1
3vil-c0d3r <92834229+3vil-c0d3r@users.noreply.github.com>
2021-10-19 21:53:47 -0400

    Add files via upload

---
+++ can_YU_fl4g_m3.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+import base64
+
+secret_word = "Y293YWJ1bmdh"
+
+print(base64.b64decode(secret_word))
+
+asdwe = "YU"
+print("I want to hack the world!")
+ncvnigir = "n3v3r_g3ts"
+reply_1 = input("Do you?").lower()
+if reply_1 == "no":
+	print("Be Gone!")
+	exit()
+elif reply_1 == "yes":
+	print("Hmm, I might have something to help you...")
+else:
+	print("Don't play games with me!")
+	exit()
+dhuijods = "_oLd}"
+reply_2 = input("But first let me ask you...whats your name?").lower()
+print("Hello {}".format(reply_2))
+nfanasd = "{oS1nT_"
+print("If we are going to continue to be friends, you must tell me the secret word\n\n")
+answer = input("Whats the secret word?").lower()
+if answer == base64.b64decode(secret_word).decode("utf-8"):
+	print("Excellent...")
+	print("Welcome to have you on board")
+else:
+	print("Be Gone!")
+	exit()
+ncvnigir = "n3v3r_g3ts"
+print("""The final step before I take over the world 
+		is to finish my masters degree in cybersecurity.
+		Once this is done I will have all the skills in place
+		to take over!!""")
+print("But first...\n")
+print("{}{}{}{}".format(asdwe,nfanasd,ncvnigir,dhuijods))
+
+
